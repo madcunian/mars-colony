435a13ee2658e02020b48ec42bf62da8d802fc8a
Stanislav Maksarov <maksarov.s@gmail.com>
2017-06-01 10:05:58 -0700

    Add form tools into register component

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -3,6 +3,14 @@ import { Job } from '../../models/job';
 import { JobsService } from '../../services/job.service';
 import { Colonist } from '../../models/colonist';
 import { ColonistService } from '../../services/colonist.service';
+import { 
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators, 
+  ValidatorFn,
+  AbstractControl
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
